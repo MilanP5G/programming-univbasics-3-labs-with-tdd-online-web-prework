6e9ecaef1db323cc042639e7a17cb10777fe94b3
Milan Parmar <milan_parmar@hotmail.co.uk>
2019-11-13 17:39:02 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,4 +4,3 @@ second_number = 18
 sum = "first_number" + "second_number"
 difference = "first_number" - "second_number"
 product = "first_number" * "second_number"
-quotient = "first_number" / "second_number"
